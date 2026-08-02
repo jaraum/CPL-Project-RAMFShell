2deaadcdcbd7f5d2836ea155686b06d38adfffb9
wangjiacheng <staubregen@outlook.com>
2026-08-02 22:10:18 +0800

    测试init_ramfs并通过

--- main.c
+++ main.c
@@ -6,27 +6,10 @@
 extern node *root;
 const char *content = "export PATH=$PATH:/usr/bin/\n";
 const char *ct = "export PATH=/home:$PATH";
-int main() {
+
+int main(void) {
   init_ramfs();
-
-  smkdir("/home");
-  smkdir("/home/ubuntu");
-  smkdir("/usr");
-  smkdir("/usr/bin");
-  stouch("/home/ubuntu/.bashrc");
-  rwrite(ropen("/home/ubuntu/.bashrc", O_WRONLY), content, strlen(content));
-  rwrite(ropen("/home/ubuntu/.bashrc", O_WRONLY | O_APPEND), ct, strlen(ct));
-  scat("/home/ubuntu/.bashrc");
-
-  init_shell();
-  swhich("ls");
-  stouch("/usr/bin/ls");
-  swhich("ls");
-  stouch("/home/ls");
-  swhich("ls");
-  secho("hello world");
-  secho("The Environment Variable PATH is:\\$PATH");
-  close_ramfs();
-  close_shell();
-  assert(root==NULL);
+  assert(root != NULL);
+  assert(root->type == DIR_NODE);
+  assert(strcmp(root->name, "/") == 0);
 }
